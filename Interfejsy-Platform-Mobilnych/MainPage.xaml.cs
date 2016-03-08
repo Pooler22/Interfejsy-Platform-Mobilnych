8be5e8108551f1bba94049ce6f68cfb4d02ec392
pooler22 <rudypawel12@gmail.com>
2016-03-08 23:34:14 +0100

    prepare views

--- Interfejsy-Platform-Mobilnych/MainPage.xaml.cs
+++ Interfejsy-Platform-Mobilnych/MainPage.xaml.cs
@@ -1,30 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Interfejsy_Platform_Mobilnych
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            mainFrame.Navigate(typeof(Details));
+            MySplitView.IsPaneOpen = false;
+
+            BackButtonVisibility = Frame.CanGoBack ? AppViewBackButtonVisibility.Visible : AppViewBackButtonVisibility.Collapsed;
+            SystemNavigationManager.GetForCurrentView().BackRequested += BackButtonPage_BackRequested;
+            base.OnNavigatedTo(e);
+        }
+
+        public AppViewBackButtonVisibility BackButtonVisibility
+        {
+            get { return SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility; }
+            set { SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = value; }
+        }
+
+        private void BackButtonPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            OnBackRequested(sender, e);
+        }
+
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+        }
+
+        private void DetailsButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Details));
+        }
+
+        private void ExchangeRateListItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            mainFrame.Navigate(typeof(Details));
+            MySplitView.IsPaneOpen = false;
+        }
+
+        private void ExitListItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Application.Current.Exit();
+        }
+
     }
 }
