a3c2be8ae1b661b3d9a511d762e613468605d5b2
pooler22 <rudypawel12@gmail.com>
2016-03-15 11:29:14 +0100

    fix

---
+++ Interfejsy-Platform-Mobilnych/ViewModel/DayViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Interfejsy_Platform_Mobilnych.Models;
+using System.Collections.ObjectModel;
+using Interfejsy_Platform_Mobilnych.Modules;
+using System.Collections.Generic;
+
+namespace Interfejsy_Platform_Mobilnych.ViewModel
+{
+    class DayViewModel
+    {
+        internal ObservableCollection<Table> tables = new ObservableCollection<Table>();
+        public ObservableCollection<Table> Tables { get { return tables; }}
+        
+        internal async void Init(List<string> codes)
+        {
+            string patternURL = "http://www.nbp.pl/kursy/xml/";
+            string patternFileExtension = ".xml";
+
+            foreach (string code in codes)
+            {
+                string output = await Downloader.GetString(patternURL + code + patternFileExtension);
+                tables.Add(DeserializerXML.deserialize(code, output));       
+            }
+        }
+    }
+}
